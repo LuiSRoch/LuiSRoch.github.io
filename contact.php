e1084e06ff84466e49b3275bf4fe14488e6372f6
MiniGeek <29665541+LuisFRoch@users.noreply.github.com>
2019-03-18 12:04:29 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -19,7 +19,8 @@ if($_POST) {
     }
     
     else {
-        $recipient = "https://mail.google.com/mail/u/1/gugolwifi@gmail.com";
+        #$recipient = "https://mail.google.com/mail/u/1/gugolwifi@gmail.com";
+        $to ='gugolwifi@gmail.com';
     }
      
     $headers  = 'MIME-Version: 1.0' . "\r\n"
